08c9009b0f285ee08ebd95a1c0418eac08dfaf63
GabeRoze <gebe1987@gmail.com>
2015-11-08 23:47:51 -0500

    testing new constraints

--- RSKImageCropper/RSKImageCropViewController.m
+++ RSKImageCropper/RSKImageCropViewController.m
@@ -33,7 +33,7 @@
 static const CGFloat kPortraitCircleMaskRectInnerEdgeInset = 15.0f;
 static const CGFloat kPortraitSquareMaskRectInnerEdgeInset = 20.0f;
 static const CGFloat kPortraitMoveAndScaleLabelVerticalMargin = 64.0f;
-static const CGFloat kPortraitCancelAndChooseButtonsHorizontalMargin = 130.0f;
+static const CGFloat kPortraitCancelAndChooseButtonsHorizontalMargin = 13.0f;
 static const CGFloat kPortraitCancelAndChooseButtonsVerticalMargin = 21.0f;
 
 static const CGFloat kLandscapeCircleMaskRectInnerEdgeInset = 45.0f;
